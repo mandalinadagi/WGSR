bcb226d71531d4e6be194628fbd59c45bb2a1600
mandalinadagi <ckorkmaz14@ku.edu.tr>
2024-03-01 13:31:26 +0300

    testing procedure added

--- test_image/test.py
+++ test_image/test.py
@@ -1,6 +1,7 @@
 import sys
 import os.path
 import glob
+from natsort import natsorted
 import cv2
 import numpy as np
 import torch
@@ -8,14 +9,18 @@ import architecture as arch
 import os
 
 model_path = sys.argv[1] 
-test_img_folder_name = sys.argv[2]
 device = torch.device('cuda')  # if you want to run on CPU, change 'cuda' -> cpu
 
-model_name = model_path.split('/')[-3]
-print(model_name)
+if len(sys.argv) > 2 :
+    test_img_folder_path = sys.argv[2]
+    test_img_folder = natsorted(sorted(glob.glob(test_img_folder_path + "/*.png"), key=len))
+else: 
+    test_img_folder = natsorted(sorted(glob.glob("./LR/*.png"), key=len))
 
-test_img_folder = '/kuacc/users/ckorkmaz14/datasets/benchmarks/' + test_img_folder_name + '/LR_bicubic/*'
-result_path = './results/'+ model_name + '/' + test_img_folder_name + '/'
+model_name = model_path.split('/')[-1].split(".")[0]
+print("Provided Model: ", model_name)
+
+result_path = './results/'+ model_name + '/'
 os.makedirs(result_path, exist_ok=True)
 
 model = arch.RRDB_Net(3, 3, 64, 23, gc=32, upscale=4, norm_type=None, act_type='leakyrelu', \
@@ -29,12 +34,9 @@ model = model.to(device)
 
 print('Model path {:s}. \nTesting...'.format(model_path))
 
-idx = 0
-for path in glob.glob(test_img_folder):
-    idx += 1
+for idx, path in enumerate(test_img_folder):
+    print(path)
     base = os.path.splitext(os.path.basename(path))[0][:-3]
-    print(idx, base)
-    # read image
     img = cv2.imread(path, cv2.IMREAD_COLOR)
     img = img * 1.0 / 255
     img = torch.from_numpy(np.transpose(img[:, :, [2, 1, 0]], (2, 0, 1))).float()
@@ -44,4 +46,4 @@ for path in glob.glob(test_img_folder):
     output = model(img_LR).data.squeeze().float().cpu().clamp_(0, 1).numpy()
     output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
     output = (output * 255.0).round()
-    cv2.imwrite(result_path + '{:s}_SR.png'.format(base), output)
+    cv2.imwrite(result_path + '{:s}_WGSR.png'.format(base), output)
